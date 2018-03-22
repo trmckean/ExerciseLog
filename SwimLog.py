8da150890ca2896ba7526b74b7704387989479de
TylerMcKean <mckean@pdx.edu>
2018-03-21 22:54:16 -0700

    Cleaning up code by separating into functions

--- SwimLog.py
+++ SwimLog.py
@@ -18,12 +18,8 @@ class DailySwimLog:
         return "{} {} yards {} minutes".format(self.date, self.yards, self.minutes)
 
 
-# Main function
-def main():
-    # Create a text file on the desktop to act as a database
-    # If file exists, open the file for writing
-    swim_file = open("/Users/TylerMcKean/Desktop/swimlog.txt", "a+")
-
+# Function to get standardized date string
+def getdatestring():
     # Standardize date string
     now = datetime.datetime.now()
     month = "{}".format(now.month)
@@ -32,37 +28,73 @@ def main():
     day = "{}".format(now.day)
     while len(day) < 2:
         day = "0" + day
-    dateString = "{}/{}/{}".format(month, day, now.year)
+    return "{}/{}/{}".format(month, day, now.year)
 
+
+# Function to prompt user about if they have data to add
+def promptuser_initial(dateString):
     # Prompt the user about any swimming done today
     print "Did you swim today? {}".format(dateString)
     answer = raw_input("Y/N\n")
+    if answer.upper() == "Y":
+        return True
+    else:
+        return False
 
-    if answer == "Y":
-        # Check and make sure we aren't adding data from same day
-        mm = mmap.mmap(swim_file.fileno(), 0, access=mmap.ACCESS_READ)
-        if mm.find(dateString) != -1:
-            print "Already logged swimming today"
-        else:
-            # Prompt user for how many yards and how long
-            yards = raw_input("How many yards?\n")
 
-            # Standardize written string length for increased readability
-            while len(yards) < 4:
-                yards = yards + " "
+# Function to make sure we haven't logged already today
+def checkduplicatelog(dateString, swim_file):
+    # Check and make sure we aren't adding data from same day
+    mm = mmap.mmap(swim_file.fileno(), 0, access=mmap.ACCESS_READ)
+    if mm.find(dateString) != -1:
+        print "Already logged swimming today"
+        return False
+    else:
+        return True
 
-            time = raw_input("How long did you swim for? (in minutes)\n")
 
-            # Instantiate daily Log object
-            dailyLog = DailySwimLog(dateString, yards, time)
+# Function to write user data to the swimLog text file
+def writedata(swim_file, dateString, data):
+    # Write data to file for storage
+    swim_file.write("{}    {} yards {} minutes\n".format(dateString, data["yards"], data["time"]))
 
-            # Write data to file for storage
-            swim_file.write("{}    {} yards {} minutes\n".format(dateString, yards, time))
+
+# Function to handle user prompting and input if there's data to add
+def getuserswimdata():
+    # Prompt user for how many yards and how long
+    yards = raw_input("How many yards?\n")
+
+    # Standardize written string length for increased readability
+    while len(yards) < 4:
+        yards = yards + " "
+
+    time = raw_input("How long did you swim for? (in minutes)\n")
+
+    # Dictionary to hold user data
+    data = {"yards": yards, "time": time}
+
+    return data
+
+
+# Main function
+def main():
+    # Create a text file on the desktop to act as a database
+    # If file exists, open the file for writing
+    swim_file = open("/Users/TylerMcKean/Desktop/swimlog.txt", "a+")
+
+    # Get current date in standardized string
+    dateString = getdatestring()
+
+    # Prompt user about swimming
+    if promptuser_initial(dateString) and checkduplicatelog(dateString, swim_file):
+        # Get data from user
+        data = getuserswimdata()
+        # Write user data to log file
+        writedata(swim_file, dateString, data)
 
     # Close file and end program
     print "Thank you - Exiting"
     swim_file.close()
-
 
 if __name__ == "__main__":
     main()
