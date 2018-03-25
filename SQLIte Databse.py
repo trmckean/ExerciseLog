7d987025ed8bc3ab226272682ec367dd6716a0b4
TylerMcKean <mckean@pdx.edu>
2018-03-24 19:24:03 -0700

    Added insert function for sqlite db

--- SQLIte Databse.py
+++ SQLIte Databse.py
@@ -6,10 +6,40 @@ import sqlite3
 
 
 # Class holding SQLite database
-class database:
-
+class Database:
     # Init and open/create DB file
     def __init__(self):
         self.connection = sqlite3.connect("swim_log.db")
         self.cursor = self.connection.cursor()
 
+    # Create initial table
+    def create_swim_log_table(self):
+        self.cursor.execute("""CREATE TABLE swim_logs
+                              (date TEXT, yards INTEGER, time INTEGER )
+                              """)
+
+    # Insert data into table
+    def insert(self, date, yards, time):
+        log_entry = (date, yards, time)
+        self.cursor.execute("INSERT INTO swim_logs VALUES (?, ?, ?)", log_entry)
+        self.connection.commit()
+
+
+
+
+# Main function to execute program
+def main():
+    # Testing implementation
+    db = Database()
+
+    controller = SwimLog.Controller()
+    controller.create_log()
+    controller.get_user_swim_data()
+
+    db.insert(controller.get_date_string(), controller.todays_log.get_yards(),
+              controller.todays_log.get_minutes())
+
+
+
+if __name__ == "__main__":
+    main()
--- SwimLog.py
+++ SwimLog.py
@@ -104,6 +104,14 @@ class DailySwimLog:
         pace = int(self.yards) / int(self.minutes) * 60
         return "Pace: {} yards/hour".format(pace)
 
+    # Function to get yards
+    def get_yards(self):
+        return int(self.yards)
+
+    # Function to get minutes
+    def get_minutes(self):
+        return int(self.minutes)
+
     # Override __str__ to allow printing of dailySwimLog object
     def __str__(self):
         return "{}    {} yards {} minutes    {}".format(self.date, self.yards, self.minutes, self.get_pace())
