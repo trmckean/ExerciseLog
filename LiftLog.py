69f5a65c079f6723bdac75b5cdcc9bd740d467c5
TylerMcKean <mckean@pdx.edu>
2018-04-12 21:53:03 -0700

    Cleanup LiftLog

--- LiftLog.py
+++ LiftLog.py
@@ -1,5 +1,11 @@
 # Module containing class for a weight lifting log
+# First iteration only supports specific weight lifting program
+
+# Standard imports
+import datetime
+
 
 class LiftLog:
-    def __init__(self):
-        pass
+    def __init__(self, type):
+        if type is not "leg" or type is not "push" or type is not "pull":
+            print "Invalid type - Only supporting Leg, Pull, and Push at this time"
