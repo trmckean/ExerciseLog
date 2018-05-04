7bd542220890f5535000c8caa309e21b2e9e8dd2
TylerMcKean <mckean@pdx.edu>
2018-05-03 21:03:14 -0700

    Accepting data in login, error message in base.html

--- Frontend/app/routes.py
+++ Frontend/app/routes.py
@@ -1,5 +1,5 @@
 # Imports
-from flask import render_template
+from flask import render_template, flash, redirect
 from Frontend.app import app
 from Backend.SQLiteDatabase import Database
 from Frontend.app.forms import LoginForm
@@ -16,7 +16,11 @@ def index():
                            pace=max_pace, entries=previous_entries)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title ='Sign In', form=form)
