1b3c138f930145ccce0b69366befa2a60ac41f13
TylerMcKean <mckean@pdx.edu>
2018-05-08 17:35:17 -0700

    Expanding database support with migrations and new post table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 
@@ -7,7 +8,19 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    # For convenient printing
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
-def __repr__(self):
-    return '<User {}>'.format(self.username)
+# Class representing table for posts by a user
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # For convenient printing
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
