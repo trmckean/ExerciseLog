0bfb4b268b3aabbee386d40b7ca6307d410ff052
TylerMcKean <mckean@pdx.edu>
2018-05-10 21:22:29 -0700

    Logging in/Out and redirecting working

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,22 @@
 # Imports
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
 from app import app
 from Backend.SQLiteDatabase import Database
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     db = Database("Backend/swim_log.db")
     max_pace = db.get_max_pace()
     previous_entries = db.get_all_entries()
-    user = {'username': 'Tyler'}
-    return render_template('index.html', title='Home', user=user,
-                           pace=max_pace, entries=previous_entries)
+    return render_template('index.html', title='Home Page',
+                           pace=max_pace, entries=previous_entries)#, posts=posts""")
 
 
 # Login page flow
@@ -31,7 +32,11 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        # Return user to page they tried to visit if not logged in, else go to index
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 # Logout functionality
