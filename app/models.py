f5d75f27d166c094ba2ada7ee4e7d9114a0410fe
TylerMcKean <mckean@pdx.edu>
2018-05-10 20:19:28 -0700

    Updating migration script to account for new posts table

--- app/models.py
+++ app/models.py
@@ -14,11 +14,12 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+
 # Class representing table for posts by a user
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     # For convenient printing
