20854f57a765990a1cad925e84b91e3407bacd3d
TylerMcKean <mckean@pdx.edu>
2018-05-10 21:02:13 -0700

    Expanded login control flow

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
 # Class representing database for users
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -34,3 +35,9 @@ class Post(db.Model):
     # For convenient printing
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+# User loader function for flask-login extension
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,8 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from Backend.SQLiteDatabase import Database
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
 
 
 @app.route('/')
@@ -16,11 +18,18 @@ def index():
                            pace=max_pace, entries=previous_entries)
 
 
+# Login page flow
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Redirect to index page if user is already logged in
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
