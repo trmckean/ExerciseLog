210b817da83df155491fc2e60d2c668d4ee918ce
TylerMcKean <mckean@pdx.edu>
2018-05-10 20:35:50 -0700

    Added flask shell support for testing db

--- exerciselog.py
+++ exerciselog.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+# Function that allows for easier testing using flask shell command
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
