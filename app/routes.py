62bca0268b27c92bd5fd3fc024446a0e7c9f83d2
TylerMcKean <mckean@pdx.edu>
2018-05-11 21:55:29 -0700

    Working registration and login with validation

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 # Imports
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
-from app import app
+from app import app, db
 from Backend.SQLiteDatabase import Database
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
@@ -16,7 +16,7 @@ def index():
     max_pace = db.get_max_pace()
     previous_entries = db.get_all_entries()
     return render_template('index.html', title='Home Page',
-                           pace=max_pace, entries=previous_entries)#, posts=posts""")
+                           pace=max_pace, entries=previous_entries)  # , posts=posts""")
 
 
 # Login page flow
@@ -39,8 +39,26 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 # Logout functionality
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+# Registration form
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Redirect to index page if user is already logged in
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
