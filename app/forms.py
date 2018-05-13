cb1e66c5cec156d8a083fcaa1a4ac7dbc4264e65
TylerMcKean <mckean@pdx.edu>
2018-05-12 22:26:28 -0700

    About me and edit profile feature working

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 # Imports
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 
@@ -32,3 +32,11 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address. Email already in use.')
+
+
+# Class representing form for users to edit profile
+# Users can edit their current username and also create a short about me section
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
